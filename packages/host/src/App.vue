6729da19738cd42be8426638bd1f4b239b666f01
zhuoqi.chen <zhuoqi_chen@126.com>
2021-12-25 19:18:17 +0800

    feat: add host vue app

---
+++ packages/host/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>Host App</h3>
+    <Button @btnClick="inc"/>
+  </div>
+</template>
+
+<script>
+import Button from 'remote_mf/Button';
+
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    inc() {
+      this.count++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ packages/host/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const { ModuleFederationPlugin } = require("webpack").container;
+module.exports = (env = {}) => ({
+  mode: "development",
+  cache: false,
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  entry: path.resolve(__dirname, "./src/main.js"),
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".vue", ".jsx", ".js", ".json"],
+    alias: {
+      vue$: "vue/dist/vue.runtime.esm.js",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new ModuleFederationPlugin({
+      remotes: {
+        remote_mf: "remote_mf@http://localhost:3001/remoteEntry.js",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 8081,
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    },
+  },
+});
