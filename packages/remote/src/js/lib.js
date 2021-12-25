a7dec25d68d268c5baa10370f152e2f7b2755930
zhuoqi.chen <zhuoqi_chen@126.com>
2021-12-25 19:10:18 +0800

    feat: add remove vue

---
+++ packages/remote/src/js/lib.js
@@ -0,0 +1,4 @@
+export const name = "czq";
+export function getName(pre) {
+  return `${pre}-${name}`;
+}
---
+++ packages/remote/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const { ModuleFederationPlugin } = require("webpack").container;
+module.exports = (env = {}) => ({
+  mode: "development",
+  cache: false,
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  entry: path.resolve(__dirname, "./src/main.js"),
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".vue", ".jsx", ".js", ".json"],
+    alias: {
+      vue$: "vue/dist/vue.runtime.esm.js",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new ModuleFederationPlugin({
+      name: "remote_mf",
+      filename: "remoteEntry.js",
+      library: { type: "var", name: "remote_mf" },
+      exposes: {
+        "./lib": "./src/js/lib.js",
+        "./Button": "./src/components/Button.vue",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 3001,
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    },
+  },
+});
